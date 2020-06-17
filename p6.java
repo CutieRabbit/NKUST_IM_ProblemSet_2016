d9ba0b12c6bc4a04a6a49e210ea585423a557b58
Xuan <52220077+CutieRabbit@users.noreply.github.com>
2020-06-17 09:28:58 +0800

    Update p6.java

--- p6.java
+++ p6.java
@@ -2,14 +2,19 @@ import java.util.Scanner;
 
 public class Main {
 	
+	static int gcd(int a, int b) {
+		if(b == 0) return a;
+		while((a %= b) > 0 && (b %= a) > 0);
+		return a+b;
+	}
+	
 	public static void main(String[] args) {
 		Scanner cin = new Scanner(System.in);
 		String data = cin.next();
 		String[] split = data.split(",");
 		int a = Integer.parseInt(split[0]);
 		int b = Integer.parseInt(split[1]);
-		while((a%=b)>0 && (b%=a)>0);
-		System.out.println(a);
+		int c = gcd(a,b);
+		System.out.println(c);
 	}
-	
 }
